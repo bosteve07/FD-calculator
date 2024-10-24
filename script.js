c3f65af03f60842b6246449e9a087377afe301c4
Bolaji <bolajioluwatobi7@gmail.com>
2024-10-24 09:55:42 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// To calculate the interest 
+function calculateTheInterest(event) {
+    event.preventDefault(); // Prevent page refresh or form submission
+    
+    const principal = parseFloat(document.getElementById('principal').value);
+    const rate = parseFloat(document.getElementById('rate').value);
+    const tenure = parseFloat(document.getElementById('tenure').value);
+
+    // Checking for NaN instead of empty string
+    if (isNaN(principal) || isNaN(rate) || isNaN(tenure)) {
+        alert("Please fill all the input fields.");
+    } else {
+        // Computation process
+        const compute = (principal * rate * tenure) / 100;
+        const total = compute + principal;
+        document.getElementById('result').innerHTML = `The Maturity Amount: N${compute.toFixed(2)}`;
+        document.getElementById('total').innerHTML = `The Total Payable: N${total.toFixed(2)}`;
+    }
+}
+
+document.getElementById('submit').addEventListener('click', calculateTheInterest);
